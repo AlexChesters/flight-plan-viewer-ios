9e9caf302fd404526e9e82906c1da82c02e68c36
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-02-02 23:19:46 +0000

    add ios app

---
+++ flight-plan-viewer/flight-plan-viewer/flight_plan_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_plan_viewerApp.swift
+//  flight-plan-viewer
+//
+//  Created by Alex Chesters on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct flight_plan_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
