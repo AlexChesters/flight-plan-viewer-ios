18ab1b8194a51f0736da382517336170b1aebb06
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-02-04 01:18:57 +0000

    add default view

--- flight-plan-viewer/flight-plan-viewer/views/SimbriefView.swift
+++ flight-plan-viewer/flight-plan-viewer/views/SimbriefView.swift
@@ -7,26 +7,38 @@
 
 import SwiftUI
 
+fileprivate struct NoUserView: View {
+    var body: some View {
+        VStack {
+            Text("Simbrief Pilot ID has not been set (enter this in the Settings tab)")
+        }
+    }
+}
+
 struct SimbriefView: View {
     @EnvironmentObject var userController: UserController
     @State var flightPlan: FlightPlan?
     
-    // TODO: conditionally render an entirely different view if pilot ID has not been set
+    @ViewBuilder
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Text(userController.simbriefUser.pilotId ?? "Simbrief Pilot ID not set")
-            
-            Text(flightPlan?.origin ?? "foo")
-            
-            Spacer()
-            
-            Divider()
-        }
-        .task {
-            await userController.simbriefUser.fetchLatestFlightPlan() { response in
-                flightPlan = response
+        if userController.simbriefUser.pilotId == nil {
+            NoUserView()
+        } else {
+            VStack {
+                Spacer()
+                
+                Text(userController.simbriefUser.pilotId ?? "Simbrief Pilot ID not set")
+                
+                Text(flightPlan?.origin ?? "foo")
+                
+                Spacer()
+                
+                Divider()
+            }
+            .task {
+                await userController.simbriefUser.fetchLatestFlightPlan() { response in
+                    flightPlan = response
+                }
             }
         }
     }
@@ -34,6 +46,13 @@ struct SimbriefView: View {
 
 struct SimbriefView_Previews: PreviewProvider {
     static var previews: some View {
-        SimbriefView()
+        let userController = UserController()
+        let userControllerWithPilotIdSet = UserController()
+        userControllerWithPilotIdSet.simbriefUser.pilotId = "Sully Sullenberger"
+        
+        return Group {
+            SimbriefView().environmentObject(userController)
+            SimbriefView().environmentObject(userControllerWithPilotIdSet)
+        }
     }
 }
